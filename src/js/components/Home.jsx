0ef2ec4c9a13b70afa051b646f44466a9e17f455
Andrea Mujica <andreamm0714@gmail.com>
2026-05-30 01:40:25 +0000

    Fixed the whole layout to work better

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,42 +5,16 @@ import { SobreMi } from "./SobreMi.jsx";
 import { ReservaConsulta } from "./ReservaConsulta.jsx";
 
 export const Home = () => {
-    useEffect(() => {
-        const spyElement = document.querySelector('[data-bs-spy="scroll"]');
-
-        if (spyElement) {
-            const bootstrap = window.bootstrap;
-            if (bootstrap && bootstrap.Scroll) {
-                new bootstrap.Scroll(spyElement, { target: "#navbar" });
-            }
-        }
-    }, []);
-
     return (
         <div className="min-vh-100 bg-light">
             <Navbar />
 
-            {/* Scroll Container */}
-            <div className="scroll-container"
-                data-bs-spy="scroll"
-                data-bs-target="#navbar"
-                data-bs-root-margin="0px 0px -40%"
-                data-bs-smooth-scroll="true"
-                style={{
-                    position: "relative",
-                    height: "100vh",
-                    overflowY: "scroll",
-                    backgroundColor: "aliceblue",
-                }}
-                tabIndex="0"
-            >
-
-                {/* Sections here */}
+            <main>
                 <Jumbotron />
                 <SobreMi />
                 <ReservaConsulta />
-
-            </div>
+            </main>
+            
         </div>
     );
 };
